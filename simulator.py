5f15dd7a086693508040e8cc0e3cef02ac3ba878
Rebecca Griffin <reb.m.griffin@gmail.com>
2019-07-21 14:04:50 +0100

    Created main method and moved code into a function

--- simulator.py
+++ simulator.py
@@ -1,17 +1,23 @@
 from random import randint
 import sys
 
-if len(sys.argv) <= 1:
-    max_dice = 9
-else:
-    try:
-        max_dice = int(sys.argv[1])
-    except ValueError:
-        raise ValueError("Please make sure you submit a whole number e.g.: python %s 6" % sys.argv[0])
 
-repeat = True
+def main(args):
+    if len(args) <= 1:
+        max_dice = 9
+    else:
+        try:
+            max_dice = int(args[1])
+        except ValueError:
+            raise ValueError("Please make sure you submit a whole number e.g.: python %s 6" % args[0])
 
-while repeat:
-    print("You rolled ",randint(1, max_dice))
-    print("Do you want to roll again?")
-    repeat = ("y" or "yes") in input().lower()
+    repeat = True
+
+    while repeat:
+        print("You rolled ", randint(1, max_dice))
+        print("Do you want to roll again?")
+        repeat = ("y" or "yes") in input().lower()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
