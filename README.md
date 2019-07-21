ee71dc68c72ba30ea5ecdbb4006194a8ccff5fa8
Rebecca Griffin <reb.m.griffin@gmail.com>
2019-07-21 13:43:21 +0100

    Updated README file Added command line arguments using sys module

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dice Roll Simulator
+Python 3.7.2
+
+Command line arguments
--- simulator.py
+++ simulator.py
@@ -1,8 +1,17 @@
 from random import randint
+import sys
+
+if len(sys.argv) <= 1:
+    max_dice = 9
+else:
+    try:
+        max_dice = int(sys.argv[1])
+    except ValueError:
+        raise ValueError("Please make sure you submit a whole number e.g.: python %s 6" % sys.argv[0])
 
 repeat = True
 
 while repeat:
-    print("You rolled ",randint(1,9))
+    print("You rolled ",randint(1, max_dice))
     print("Do you want to roll again?")
     repeat = ("y" or "yes") in input().lower()
